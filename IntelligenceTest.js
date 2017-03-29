c9846650c487f041e5e4951dd00b7f93a151b13c
Osar Fullerton <ofullerton@mail.csuchico.edu>
2017-03-29 10:03:05 -0700

    fooled around with the validation tools

--- IntelligenceTest.js
+++ IntelligenceTest.js
@@ -3,25 +3,23 @@ function continueButton (){
  }
 
 
-function validateAnswer (){
+function validateAnswer(){
   var  answerEntered = document.getElementById("answer").value;
-  var x = document.getElementById('answer')
                   //this is where we choose the value we want
 
 
   if (answerEntered = 1738)
   {
-
     document.getElementById("answerGroup").classList.add("has-success");
     document.getElementById("answerGroup").classList.remove("has-error");
     document.getElementById("answerError").classList.remove("shown-message");
-    document.getElementById("answerError").classList.add("hidden-message");
+    document.getElementById("answerSuccess").classList.add("hidden-message");
     }
-    else (answerEntered != 1738) {
+    else (answerEntered !== 1738) {
     document.getElementById("answerGroup").classList.add("has-error");
     document.getElementById("answerGroup").classList.remove("has-success");
     document.getElementById("answerError").innerHTML="Answer is not correct.";
-    document.getElementById("answerError").classList.remove("hidden-message");
+    document.getElementById("answerSuccess").classList.remove("hidden-message");
     document.getElementById("answerError").classList.add("shown-message");
 }
 }
