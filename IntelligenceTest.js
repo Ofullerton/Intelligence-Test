c0971f1bf7276ed058d983c279bd3b13caef725f
Osar Fullerton <ofullerton@mail.csuchico.edu>
2017-03-30 16:26:25 -0700

    staging before the buttons decide not to work again

--- IntelligenceTest.js
+++ IntelligenceTest.js
@@ -1,17 +1,32 @@
-function continueButton(){
-window.location.href = "index2.html";
+function continueButton()
+{
+window.location.href="index2.html";
+// setCookie("correct",2,1);
 }
-function continueButton1(){
- window.location.href = "index3.html";
+function continueButton1()
+{
+ window.location.href ="index3.html";
+ getCookie("correct",2,1);
+ setCookie("correct",2,1);
  }
-function continueButton2(){
-  window.location.href = "index4.html";
+function continueButton2()
+{
+  window.location.href ="index4.html";
+  getCookie("correct",2,1);
+  setCookie("correct",2,1);
   }
-function continueButton3(){
-  window.location.href = "index5.html";
+function continueButton3()
+{
+  window.location.href ="index5.html";
+  getCookie("correct",2,1);
+  setCookie("correct",2,1);
   }
-
-function validateAnswer(){
+function continueButton3()
+{
+  window.location.href ="index5.html";
+  getCookie("correct",2,1);
+  }
+function validateAnswer() {
   var  x = document.getElementById("answer").value;
                   //this is where we choose the value we want
   if (x == 2243)
@@ -49,24 +64,26 @@ function validateAnswer2(){
 }
 //Generously borrowed from w3 https://www.w3schools.com/js/js_cookies.asp
 //function setCookie(cname, cvalue, exdays) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-//     var expires = "expires="+ d.toUTCString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+//     function getCookie(cname) {
+// function setCookie(cname, cvalue, exdays) {
+//   var d = new Date();
+//   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+//   var expires = "expires="+d.toUTCString();
+//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// //Generously borrowed from w3 https://www.w3schools.com/js/js_cookies.asp
+// //function getCookie(cname) function checkCookie() {
+// function getCookie(cname) {
+//   var name = cname + "=";
+//   var decodedCookie = decodeURIComponent(document.cookie);
+//   var ca = decodedCookie.split(';');
+//   for(var i = 0; i <ca.length; i++)
+//   {
+//       var c = ca[i];
+//       while (c.charAt(0) == ' ') {
+//           c = c.substring(1);
+//       }
+//       if (c.indexOf(name) == 0) {
+//           return c.substring(name.length, c.length);
+//       }
+//   }
 // }
-//Generously borrowed from w3 https://www.w3schools.com/js/js_cookies.asp
-//function getCookie(cname) {
-  //  var results = results + "=";
-    //var decodedCookie = decodeURIComponent(document.cookie);
-    //var ca = decodedCookie.split(';');
-    //for(var i = 0; i <ca.length; i++) {
-      //  var c = ca[i];
-        //while (c.charAt(0) == ' ') {
-          //  c = c.substring(1);
-        //}
-      //  if (c.indexOf(name) == 0) {
-        //    return c.substring(name.length, c.length);
-      //  }
-  //  }
-  //  return "";
-//}
