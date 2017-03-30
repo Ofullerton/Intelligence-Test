a7ac3fd437339640cd6c49e3dbea40cd7aefec5c
Osar Fullerton <ofullerton@mail.csuchico.edu>
2017-03-30 13:08:27 -0700

    Successfully got the continue button to work the long way.
    Still can't get the instant validator to display the correct messages. Positive the code is cursed and wants me to get a bad grade.

--- IntelligenceTest.js
+++ IntelligenceTest.js
@@ -1,12 +1,19 @@
 function continueButton(){
- window.location.href = "index2.html";
+window.location.href = "index2.html";
+}
+function continueButton1(){
+ window.location.href = "index3.html";
  }
-
-
+function continueButton2(){
+  window.location.href = "index4.html";
+  }
+function continueButton3(){
+  window.location.href = "index5.html";
+  }
+  
 function validateAnswer(){
   var  answerEntered = document.getElementById("answer").value;
                   //this is where we choose the value we want
-
 
   if (answerEntered = 1738)
   {
@@ -14,6 +21,7 @@ function validateAnswer(){
     document.getElementById("answerGroup").classList.remove("has-error");
     document.getElementById("answerError").classList.remove("shown-message");
     document.getElementById("answerSuccess").classList.add("hidden-message");
+    document.getElementById("answerSuccess").innerHTML="Answer is correct.";
     }
     else (answerEntered !== 1738)
   {
