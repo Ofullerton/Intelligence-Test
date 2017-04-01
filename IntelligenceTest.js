271d285bb280400532dfc0b1118b76c5b0016a9b
Osar Fullerton <ofullerton@mail.csuchico.edu>
2017-03-31 19:08:38 -0700

    Done

--- IntelligenceTest.js
+++ IntelligenceTest.js
@@ -1,31 +1,35 @@
 function continueButton()
 {
-window.location.href="index2.html";
+window.location.href ="index2.html";
 // setCookie("correct",2,1);
 }
 function continueButton1()
 {
  window.location.href ="index3.html";
- getCookie("correct",2,1);
- setCookie("correct",2,1);
+ getCookie("correct",2 ,1);
+ setCookie("correct",2 ,1);
  }
 function continueButton2()
 {
   window.location.href ="index4.html";
-  getCookie("correct",2,1);
-  setCookie("correct",2,1);
+  getCookie("correct",2 ,1);
+  setCookie("correct",2 ,1);
   }
 function continueButton3()
 {
   window.location.href ="index5.html";
-  getCookie("correct",2,1);
-  setCookie("correct",2,1);
+  getCookie("correct",2 ,1);
+  setCookie("correct",2 ,1);
   }
 function continueButton3()
 {
   window.location.href ="index5.html";
-  getCookie("correct",2,1);
+  getCookie("correct",2 ,1);
+
   }
+function continueButton4(){
+  var total = document.cookie;
+}
 function validateAnswer() {
   var  x = document.getElementById("answer").value;
                   //this is where we choose the value we want
@@ -45,9 +49,28 @@ function validateAnswer() {
 }
 }
 function validateAnswer2(){
-  var  x = document.getElementById("answer2").value;
+  var  y = document.getElementById("answer2").value;
                   //this is where we choose the value we want
-  if (x == 352)
+  if (y == 352)
+  {
+    document.getElementById("answerGroup").classList.add("has-success");
+    document.getElementById("answerGroup").classList.remove("has-error");
+    document.getElementById("answerError").classList.remove("shown-message");
+    document.getElementById("answerSuccess").classList.add("hidden-message");
+    }
+    else
+  {
+    document.getElementById("answerGroup").classList.add("has-error");
+    document.getElementById("answerGroup").classList.remove("has-success");
+    document.getElementById("answerSuccess").classList.remove("hidden-message");
+    document.getElementById("answerError").classList.add("shown-message");
+}
+}
+
+function validateAnswer3(){
+  var  z = document.getElementById("answer2").value;
+                  //this is where we choose the value we want
+  if (z == 5)
   {
     document.getElementById("answerGroup").classList.add("has-success");
     document.getElementById("answerGroup").classList.remove("has-error");
@@ -63,27 +86,26 @@ function validateAnswer2(){
 }
 }
 //Generously borrowed from w3 https://www.w3schools.com/js/js_cookies.asp
-//function setCookie(cname, cvalue, exdays) {
-//     function getCookie(cname) {
-// function setCookie(cname, cvalue, exdays) {
-//   var d = new Date();
-//   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//   var expires = "expires="+d.toUTCString();
-//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// //Generously borrowed from w3 https://www.w3schools.com/js/js_cookies.asp
-// //function getCookie(cname) function checkCookie() {
-// function getCookie(cname) {
-//   var name = cname + "=";
-//   var decodedCookie = decodeURIComponent(document.cookie);
-//   var ca = decodedCookie.split(';');
-//   for(var i = 0; i <ca.length; i++)
-//   {
-//       var c = ca[i];
-//       while (c.charAt(0) == ' ') {
-//           c = c.substring(1);
-//       }
-//       if (c.indexOf(name) == 0) {
-//           return c.substring(name.length, c.length);
-//       }
-//   }
-// }
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//Generously borrowed from w3 https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
